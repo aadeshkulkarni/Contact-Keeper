4babd902b618508a057c52e7ef61b0c5c5e14839
Aadesh <aadee.kukani@gmail.com>
2020-08-26 21:02:15 +0530

    Express server setup with Initial routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const user = require("../models/User");
+
 // @route     POST api/users
 // @desc      Regiter a user
 // @access    Public
-router.post("/", (req, res) => res.send(" Register a user"));
+router.post("/", (req, res) => res.send(req.body));
 
 module.exports = router;
